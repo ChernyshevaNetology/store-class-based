467994894115b2d6faad7adfdc5e352adc408a67
ChernyshevaNetology <chernysheva87@mail.ru>
2020-10-16 10:50:00 +0200

    Updates

---
+++ precache-manifest.723b09532ce9d2d6d409b13c73f9e56d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b407313a739b8e9d31f4d1bf941cbca1",
+    "url": "/store-class-based/index.html"
+  },
+  {
+    "revision": "0f8faa44c9ea802d70e5",
+    "url": "/store-class-based/static/css/main.329c60eb.chunk.css"
+  },
+  {
+    "revision": "2cca99945399ff4cd85b",
+    "url": "/store-class-based/static/js/2.77f2e939.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/store-class-based/static/js/2.77f2e939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8faa44c9ea802d70e5",
+    "url": "/store-class-based/static/js/main.da7a80d4.chunk.js"
+  },
+  {
+    "revision": "50f2138e124cae525a39",
+    "url": "/store-class-based/static/js/runtime-main.57f8a46c.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/store-class-based/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/store-class-based/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
